801c6f6d6deed64b589f0298a567b58989433b79
AlfieriChou <alfierichou@gmail.com>
2020-09-19 16:41:29 +0800

    chore: update

--- kuaishou/adLoginByChildAccountId.js
+++ kuaishou/adLoginByChildAccountId.js
@@ -2,7 +2,38 @@ const puppeteer = require('puppeteer')
 
 const cookies = []
 
-const bootstrap = async (accountId) => {
+const getAccounts = page => {
+  return new Promise(async (resolve, reject) => {
+    await page.on('response', async res => {
+      const url = res.url()
+      if (url === 'https://uc.e.kuaishou.com/rest/web/getAccountList') {
+        if (res.status() === 200 && res.ok()) {
+          const ret = JSON.parse(await res.text())
+          if (ret.result !== 1) {
+            reject('Error')
+          }
+          resolve(ret.accountList.ucAccountInfos)
+        }
+      }
+    })
+  })
+}
+
+const getCookies = page => {
+  return new Promise(async (resolve, reject) => {
+    await page.on('response', async res => {
+      const url = res.url()
+      if (url === 'https://ad.e.kuaishou.com/rest/dsp/hitLine/account') {
+        if (res.status() === 200 && res.ok()) {
+          const data = await page._client.send('Network.getAllCookies')
+          resolve(data)
+        }
+      }
+    })
+  })
+}
+
+const bootstrap = async () => {
   const browser = await puppeteer.launch({
     timeout: 10000,
     devtools: true,
@@ -14,45 +45,48 @@ const bootstrap = async (accountId) => {
     await page.setCookie(...cookies)
     await page.goto(
       'https://uc.e.kuaishou.com/?sid=kuaishou.ad.dsp&followUrl=https%3A%2F%2Fad.e.kuaishou.com%2Findex#/index',
-      { waitUntil: 'networkidle2' }
+      // { waitUntil: 'networkidle2' }
     )
-    await page.type(
-      '#app > div > div.body-wrap > div > div.search > span > span > input',
-      `${accountId}`
-    )
-    await page.$$eval(
-      '#app > div > div.body-wrap > div > div.search > span > span > span > button',
-      async anchors => {
-        await anchors.reduce(async (promise, anchor) => {
-          await promise
-          if (anchor.textContent == '搜 索') {
-            await anchor.click()
-          }
-        }, Promise.resolve())
-      }
-    )
-    await page.$$eval(
-      '#app > div > div.body-wrap > div > div.uc-table > div > div > div > div > div > div > table > tbody > tr > td:nth-child(4) > a',
-      async anchors => {
-        await anchors.reduce(async (promise, anchor) => {
-          await promise
-          if (anchor.textContent == '进入') {
-            await anchor.click()
-          }
-        }, Promise.resolve())
-      }
-    )
-    await page.on('response', async res => {
-      const url = res.url()
-      if (url === 'https://ad.e.kuaishou.com/rest/dsp/hitLine/account') {
-        if (res.status() === 200 && res.ok()) {
-          const data = await page._client.send('Network.getAllCookies')
-          console.log('------->', data, await res.text())
-          await page.close()
-          await browser.close()
+    const accounts = await getAccounts(page)
+    let index = 0
+    while (index < accounts.length) {
+      const { accountId } = accounts[index]
+      await page.goto(
+        'https://uc.e.kuaishou.com/?sid=kuaishou.ad.dsp&followUrl=https%3A%2F%2Fad.e.kuaishou.com%2Findex#/index',
+        { waitUntil: 'networkidle2' }
+      )
+      await page.type(
+        '#app > div > div.body-wrap > div > div.search > span > span > input',
+        `${accountId}`
+      )
+      await page.$$eval(
+        '#app > div > div.body-wrap > div > div.search > span > span > span > button',
+        async anchors => {
+          await anchors.reduce(async (promise, anchor) => {
+            await promise
+            if (anchor.textContent == '搜 索') {
+              await anchor.click()
+            }
+          }, Promise.resolve())
         }
-      }
-    })
+      )
+      await page.$$eval(
+        '#app > div > div.body-wrap > div > div.uc-table > div > div > div > div > div > div > table > tbody > tr > td:nth-child(4) > a',
+        async anchors => {
+          await anchors.reduce(async (promise, anchor) => {
+            await promise
+            if (anchor.textContent == '进入') {
+              await anchor.click()
+            }
+          }, Promise.resolve())
+        }
+      )
+      const cookies = await getCookies(page)
+      console.log(`----${index}--->`, JSON.stringify(cookies))
+      index += 1
+    }
+    await page.close()
+    await browser.close()
   } catch (err) {
     await page.close()
     await browser.close()
@@ -60,4 +94,4 @@ const bootstrap = async (accountId) => {
   }
 }
 
-bootstrap(123456)
+bootstrap()
